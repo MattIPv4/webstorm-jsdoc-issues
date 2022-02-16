af44e376e6203f78a8b8b0a9fd115c2412899063
MattIPv4 <me@mattcowley.co.uk>
2022-02-16 13:30:56 +0000

    Library issue

---
+++ 4/test.js
@@ -0,0 +1,6 @@
+/**
+ * @type {import('markdown-it/lib/parser_inline').RuleInline}
+ */
+const testing = (a, b) => false;
+
+// Bad: `RuleInline` shows as unresolved variable or type (VSCode automatically pulls it from the correct types library)
